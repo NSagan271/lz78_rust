25fa61998d7c1399f26ded83c0967d198a7f64a1
Naomi Sagan <nsagan@stanford.edu>
2025-04-14 14:29:40 -0700

    Fix bug in train_spa.py

--- python_experiments/train_spa.py
+++ python_experiments/train_spa.py
@@ -5,11 +5,13 @@ import os
 
 
 MAX_SIZE = 15 * 1e9
+NUM_DOC = 5_000_000
+SAVE_FREQ = 10_000
 
 def main():
     model = TokenizedLZPlusEmbedding( 
         inner_model_name="BAAI/bge-base-en-v1.5",
-        output_dir="data/object",
+        output_dir="data/object2",
         compute_device="cuda:7",
         weight_type=WeightType.LOG_LOSS,
         pca_dim=256,
@@ -17,16 +19,18 @@ def main():
         overwrite_objects=True
     )
 
-    data_reader = ParquetReader("hf://datasets/HuggingFaceFW/fineweb/sample/10BT", limit=5_000_000) 
-    for document in tqdm(data_reader()):
+    data_reader = ParquetReader("hf://datasets/HuggingFaceFW/fineweb/sample/10BT", limit=NUM_DOC) 
+    for (i, document) in enumerate(tqdm(data_reader())):
         # do something with document
         model.spa.reset_state()
-        model.train_spa(document.text)
+        save = (i % SAVE_FREQ) == SAVE_FREQ - 1
+        model.train_spa(document.text, save=save)
 
-        if os.path.getsize(model.spa_file) > MAX_SIZE:
+        if save and os.path.getsize(model.spa_file) > MAX_SIZE:
             print("SPA reached max size! Exiting train loop.")
             break
     model.spa.prune(2)
+    model.spa.to_file(model.spa_file)
 
 if __name__ == "__main__":
     main()
