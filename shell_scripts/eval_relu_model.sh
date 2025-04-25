039de0a241292dafe9cb9481c884671bcc443180
Naomi Sagan <nsagan@stanford.edu>
2025-04-25 00:31:06 -0700

    Remove embedding stuff

--- shell_scripts/eval_relu_model.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python scripts/evaluate_model.py \
-    --model-type relu \
-    --model-save-dir data/object/relu
--- shell_scripts/train_ngram_model_no_pca.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python scripts/train_model.py \
-    --model-type ngram \
-    --model-save-dir data/object/ngram_no_pca \
-    --no-ngram-pca \
